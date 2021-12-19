31253f4e7aac0999676c59d670271bd45d172796
Miftahul Dewanto <90891230+MiftahulDewanto@users.noreply.github.com>
2021-12-20 00:09:51 +0700

    uas_webpage.php
    Web Page 1 (PHP)

---
+++ uas_webpage.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8"/>
+<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Web 1920 – 1</title>
+<link rel="stylesheet" type="text/css" id="applicationStylesheet" href="index.css">
+<script id="applicationScript" type="text/javascript" src="index.js"></script>
+</head>
+<body>
+	<menu>
+		<ul>
+			<li><a href="">Home</a><li>
+			<li><a href="Web2.html">About</a><li>
+			<li><a href="">Portofolio</a><li>
+			<li><a href="">Contact Us</a><li>
+			<li><a href="login.php">Log Out</a></li>	
+		</ul>
+	</menu>
+<div id="Web_1920__1">
+	<svg class="Polygon_2" viewBox="0 0 282 248">
+		<path id="Polygon_2" d="M 140.9999847412109 0 L 282 248 L 0 248 Z">
+		</path>
+	</svg>
+	<svg class="Polygon_3" viewBox="0 0 282 248">
+		<path id="Polygon_3" d="M 140.9999847412109 0 L 282 248 L 0 248 Z">
+		</path>
+	</svg>
+	<svg class="Polygon_4" viewBox="0 0 282 248">
+		<path id="Polygon_4" d="M 140.9999847412109 0 L 282 248 L 0 248 Z">
+		</path>
+	</svg>
+	<svg class="Polygon_5" viewBox="0 0 282 248">
+		<path id="Polygon_5" d="M 140.9999847412109 0 L 282 248 L 0 248 Z">
+		</path>
+	</svg>
+	<svg class="Polygon_6" viewBox="0 0 282 248">
+		<path id="Polygon_6" d="M 140.9999847412109 0 L 282 248 L 0 248 Z">
+		</path>
+	</svg>
+	<svg class="Polygon_7" viewBox="0 0 282 248">
+		<path id="Polygon_7" d="M 140.9999847412109 0 L 282 248 L 0 248 Z">
+		</path>
+	</svg>
+	<svg class="Polygon_8" viewBox="0 0 282 248">
+		<path id="Polygon_8" d="M 140.9999847412109 0 L 282 248 L 0 248 Z">
+		</path>
+	</svg>
+	<svg class="Polygon_9" viewBox="0 0 282 248">
+		<path id="Polygon_9" d="M 140.9999847412109 0 L 282 248 L 0 248 Z">
+		</path>
+	</svg>
+	<img id="download_2" src="download_2.png" srcset="download_2.png 1x, download_2@2x.png 2x">
+		
+	<div id="n_mifthld">
+		<span>: @mifthl.d</span>
+	</div>
+	<div id="n_Miftahul_Dewanto">
+		<span>: Miftahul Dewanto</span>
+	</div>
+	<img id="kisspng-gmail-email-computer-i" src="kisspng-gmail-email-computer-i.png" srcset="kisspng-gmail-email-computer-i.png 1x, kisspng-gmail-email-computer-i@2x.png 2x">
+		
+	<div id="n_haloh0141gmailcom">
+		<span>: haloh0141@gmail.com</span>
+	</div>
+	<img id="download_3" src="download_3.png" srcset="download_3.png 1x, download_3@2x.png 2x">
+		
+	<div id="n__ALL_ABOUT_M_I_F_T_A_H_U_L__">
+		<span> </span><br/><span style="font-size:30px;">ALL ABOUT</span><br/><span>M I F T A H U L  D E W A N T O </span><br/>
+	</div>
+	<div id="Hey_My_name_is_Miftahul_Dewant">
+		<span>Hey! My name is Miftahul Dewanto.<br/>I am a student from the Department <br/>of Mathematics, Faculty of Science and <br/>Technology, State Islam University of <br/>Maulana Malik Ibrahim Malang.</span><br/>
+	</div>
+	<img id="WhatsApp_Image_2021-09-29_at_1" src="WhatsApp_Image_2021-09-29_at_1.png" srcset="WhatsApp_Image_2021-09-29_at_1.png 1x, WhatsApp_Image_2021-09-29_at_1@2x.png 2x">
+		
+</div>
+</body>
+</html>
