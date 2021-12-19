a3c88f4a3f49164a7077a24740a5da947f0233b3
Miftahul Dewanto <90891230+MiftahulDewanto@users.noreply.github.com>
2021-12-20 00:12:11 +0700

    uas_webpage2.php
    Web Page 2 (PHP)

---
+++ uas_webpage2.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8"/>
+<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Web 1920 – 2</title>
+<link rel="stylesheet" type="text/css" id="applicationStylesheet" href="index1.css"/>
+<script id="applicationScript" type="text/javascript" src="index1.js"></script>
+</head>
+<body>
+    <menu>
+		<ul>
+			<li><a href="index.php">Home</a><li>
+			<li><a href="login.php">Log Out</a></li>	
+		</ul>
+	</menu>
+<div id="Web_1920__2">
+	<svg class="Polygon_10" viewBox="0 0 1 4">
+		<path id="Polygon_10" d="M 0.4999999403953552 0 L 0.9999999403953552 4 L 0 4 Z">
+		</path>
+	</svg>
+	<svg class="Polygon_11" viewBox="0 0 67 52">
+		<path id="Polygon_11" d="M 33.49999618530273 0 L 33.66749954223633 34.58000183105469 L 67 52 L 33.5 34.84000015258789 L 0 52 L 33.33250045776367 34.58000183105469 Z">
+		</path>
+	</svg>
+	<div id="Component_5__1" class="Component_5___1">
+		<svg class="Polygon_12" viewBox="0 0 36 34">
+			<path id="Polygon_12" d="M 18 0 L 36 34 L 0 34 Z">
+			</path>
+		</svg>
+		<svg class="Polygon_13" viewBox="0 0 36 34">
+			<path id="Polygon_13" d="M 18 0 L 36 34 L 0 34 Z">
+			</path>
+		</svg>
+		<svg class="Polygon_14" viewBox="0 0 36 34">
+			<path id="Polygon_14" d="M 18 0 L 36 34 L 0 34 Z">
+			</path>
+		</svg>
+		<svg class="Polygon_15" viewBox="0 0 36 34">
+			<path id="Polygon_15" d="M 18 0 L 36 34 L 0 34 Z">
+			</path>
+		</svg>
+	</div>
+	<div id="B_I_O_G_R_A_P_H_Y">
+		<span>B I O G R A P H Y</span>
+	</div>
+	<svg class="Path_1" viewBox="0 0 353 5">
+		<path id="Path_1" d="M 0 0 L 353 0">
+		</path>
+	</svg>
+	<svg class="Line_2" viewBox="0 0 353 5">
+		<path id="Line_2" d="M 0 0 L 353 0">
+		</path>
+	</svg>
+	<div id="O_R_G_A_N_I_Z_A_T_I_O_N">
+		<span>O R G A N I Z A T I O N</span>
+	</div>
+	<div id="NAME__MIFTAHUL_DEWANTO_PLACE_A">
+		<span>NAME : MIFTAHUL DEWANTO<br/>PLACE AND DATE OF BIRTH : 23TH OF MARCH 2000<br/>GENDER : MALE<br/>RELIGION : ISLAM<br/>HOBBIES : TRAVELLING AND FUTSAL</span>
+	</div>
+	<div id="n_017_Member_of_Nature_Lover_T">
+		<span>2017 Member of Nature Lover Tirta Buana<br/>2019 Member of Mathematics English Club<br/>2020 Committee of Mathematics English Club Competition<br/>2020 Member of SEMATA</span>
+	</div>
+
+    <img id="png-clipart-microsoft-word-com" src="png-clipart-microsoft-word-com.png" srcset="png-clipart-microsoft-word-com.png 1x, png-clipart-microsoft-word-com@2x.png 2x">
+
+	<img id="download" src="download.png" srcset="download.png 1x, download@2x.png 2x">
+		
+	<img id="download_4" src="download_4.png" srcset="download_4.png 1x, download_4@2x.png 2x">
+		
+	<div id="S_K_I_L_L_">
+		<span>S K I L L </span>
+	</div>
+	<div id="Component_5__2" class="Component_5___2">
+		<svg class="Polygon_12_bi" viewBox="0 0 36 34">
+			<path id="Polygon_12_bi" d="M 18 0 L 36 34 L 0 34 Z">
+			</path>
+		</svg>
+		<svg class="Polygon_13_bj" viewBox="0 0 36 34">
+			<path id="Polygon_13_bj" d="M 18 0 L 36 34 L 0 34 Z">
+			</path>
+		</svg>
+		<svg class="Polygon_14_bk" viewBox="0 0 36 34">
+			<path id="Polygon_14_bk" d="M 18 0 L 36 34 L 0 34 Z">
+			</path>
+		</svg>
+		<svg class="Polygon_15_bl" viewBox="0 0 36 34">
+			<path id="Polygon_15_bl" d="M 18 0 L 36 34 L 0 34 Z">
+			</path>
+		</svg>
+	</div>
+	<div id="Component_5__3" class="Component_5___3">
+		<svg class="Polygon_12_bn" viewBox="0 0 36 34">
+			<path id="Polygon_12_bn" d="M 18 0 L 36 34 L 0 34 Z">
+			</path>
+		</svg>
+		<svg class="Polygon_13_bo" viewBox="0 0 36 34">
+			<path id="Polygon_13_bo" d="M 18 0 L 36 34 L 0 34 Z">
+			</path>
+		</svg>
+		<svg class="Polygon_14_bp" viewBox="0 0 36 34">
+			<path id="Polygon_14_bp" d="M 18 0 L 36 34 L 0 34 Z">
+			</path>
+		</svg>
+		<svg class="Polygon_15_bq" viewBox="0 0 36 34">
+			<path id="Polygon_15_bq" d="M 18 0 L 36 34 L 0 34 Z">
+			</path>
+		</svg>
+	</div>
+	<svg class="Path_2" viewBox="0 0 353 5">
+		<path id="Path_2" d="M 0 0 L 353 0">
+		</path>
+	</svg>
+		
+	<div id="n_______________Miftahul_Dewan">
+		<span>               Miftahul Dewanto</span><span style="font-style:normal;font-weight:bold;"> </span><br/><span>Jl A Satsui Tubun IV | 6285719561913 | <br/>           haloh0141@gmail.com</span>
+	</div>
+</div>
+</body>
+</html>
